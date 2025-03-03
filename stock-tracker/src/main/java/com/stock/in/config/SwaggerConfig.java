3519a9fef4a8411f242d9ffc1079885e8a13bc63
shivamupadhya1 <140012284+shivamupadhya1@users.noreply.github.com>
2025-03-04 00:27:52 +0530

    Update SwaggerConfig.java

--- stock-tracker/src/main/java/com/stock/in/config/SwaggerConfig.java
+++ stock-tracker/src/main/java/com/stock/in/config/SwaggerConfig.java
@@ -1,20 +1,17 @@
 package com.stock.in.config;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI stockTrackerOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Stock Tracker API")
-                        .version("1.0")
-                        .description("API for tracking Dow Jones Industrial Average stock prices")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+@OpenAPIDefinition(
+    info = @Info(title = "Stock Tracker API", version = "1.0", description = "API for tracking stock prices"),
+    servers = {
+        @Server(url = "http://localhost:8080/stock-tracker", description = "Local Development Server"),
+        @Server(url = "https://uat.example.com/stock-tracker", description = "UAT Server")
     }
+)
+public class SwaggerConfig {
 }
